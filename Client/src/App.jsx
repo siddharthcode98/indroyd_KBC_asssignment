7449520325ede01355308577ecaf235cde9e7a77
Vaddemani Siddharth <139576040+siddharthcode98@users.noreply.github.com>
2024-10-09 09:35:03 +0530

    update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,9 +6,9 @@ import UserDetails from "./UserDetails/UserDetails";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/qrcode" />} />
       <Route path="/qrcode" element={<Qrcode />} />
       <Route path="/" element={<UserDetails />} />
-      <Route path="/" element={<Navigate to="/qrcode" />} />
     </Routes>
   );
 }
